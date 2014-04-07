631ba9f3eefcd57da9d05e0b441a095ff5b64a9d
jbott <snowleopardjb@gmail.com>
2014-04-06 23:05:06 -0400

    Add motion sensing capabilities

--- UDPUpdater.ino
+++ UDPUpdater.ino
@@ -7,6 +7,8 @@
 #define IR_PIN 4
 #define PIR_PIN 6
 #define PACKET_SIZE 128
+#define CYCLE_MAX 20000
+#define CYCLE_MOTION_REQUIRED 10000
 
 IRrecv irrecv(IR_PIN);
 
@@ -25,6 +27,11 @@ int remote_port = 12001;
 char ReplyBuffer[PACKET_SIZE];
 PString ReplyBufferString(ReplyBuffer, sizeof(ReplyBuffer));
 char IRMessage[] = "IRCODE=";
+char MotionMessage[] = "MOTION";
+
+// counters for the motion sensor
+int cycle_count = 0;
+int cycle_motion = 0;
 
 // An EthernetUDP instance to let us send and receive packets over UDP
 EthernetUDP Udp;
@@ -37,6 +44,8 @@ void setup() {
   Serial.println(__TIMESTAMP__);
   // Start IR reciever
   irrecv.enableIRIn();
+  // Start PIR sensor
+  pinMode(PIR_PIN, INPUT);
   // Start Ethernet
   Ethernet.begin(mac, ip);
   Udp.begin(12001);
@@ -60,5 +69,20 @@ void loop() {
       ReplyBufferString.begin();
     }
     irrecv.resume(); // Receive the next value
+  }
+
+  cycle_count++;
+  if (digitalRead(PIR_PIN))
+    cycle_motion++;
+
+  if (cycle_count >= CYCLE_MAX) {
+
+    if (cycle_motion >= CYCLE_MOTION_REQUIRED) {
+      Udp.beginPacket(remote_ip, remote_port);
+      Udp.write(MotionMessage);
+      Udp.endPacket();
+    }
+    cycle_count = 0;
+    cycle_motion = 0;
   }
 }
